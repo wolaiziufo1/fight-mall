da56e512c7b4f1118e80aec04895354931486baf
wolaiziufo1 <qweasdzxc4553@163.com>
2018-11-08 15:48:43 +0800

    完成了用户的注册功能

---
+++ fight_mall/fight_mall/apps/users/serializers.py
@@ -0,0 +1,82 @@
+import re
+
+from rest_framework import serializers
+from users.models import User
+from django_redis import get_redis_connection
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    创建用户序列化器
+    """
+    password2 = serializers.CharField(label='确认密码', write_only=True)
+    sms_code = serializers.CharField(label='短信验证码', write_only=True)
+    allow = serializers.CharField(label='同意协议', write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'password', 'password2', 'sms_code', 'mobile', 'allow')
+        extra_kwargs = {
+            'username': {
+                'min_length': 5,
+                'max_length': 20,
+                'error_messages': {
+                    'min_length': '仅允许5-20个字符的用户名',
+                    'max_length': '仅允许5-20个字符的用户名',
+                }
+            },
+            'password': {
+                'write_only': True,
+                'min_length': 8,
+                'max_length': 20,
+                'error_messages': {
+                    'min_length': '仅允许8-20个字符的密码',
+                    'max_length': '仅允许8-20个字符的密码',
+                }
+            }
+        }
+
+    def validate_mobile(self, value):
+        """验证手机号"""
+        if not re.match(r'^1[3-9]\d{9}$', value):
+            raise serializers.ValidationError('手机号格式不正确')
+        return value
+
+    def validate_allow(self, value):
+        """验证同意协议"""
+        if value != 'true':
+            raise serializers.ValidationError('协议未同意')
+        return value
+
+    def validate(self, attrs):
+        """密码验证"""
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError('密码不一致')
+
+        # 短信验证
+        # 1.建立连接
+        conn = get_redis_connection('verify')
+        # 2.获取数据
+        real_sms_code = conn.get('sms_%s' % attrs['mobile'])
+        # print(real_sms_code)
+        # 3.判断数据是否超过有效期
+        if not real_sms_code:
+            raise serializers.ValidationError('短信失效')
+        # 4.转换数据
+        real_sms_code = real_sms_code.decode()
+        # 5.比对验证
+        if attrs['sms_code'] != real_sms_code:
+            raise serializers.ValidationError('短信错误')
+        return attrs
+
+    def create(self, validated_data):
+        """保存"""
+        del validated_data['password2']
+        del validated_data['sms_code']
+        del validated_data['allow']
+        user = super().create(validated_data)
+        # 调用django的认证系统加密密码
+        user.set_password(validated_data['password'])
+        user.save()
+        # print(user)
+        return user
--- fight_mall/fight_mall/apps/users/views.py
+++ fight_mall/fight_mall/apps/users/views.py
@@ -1,3 +1,38 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from rest_framework.views import APIView
+from users.models import User
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView
+from users.serializers import UserSerializer
+
 
 # Create your views here.
+# 判断用户名是否存在
+class UserNameCountView(APIView):
+    def get(self, request, username):
+        # 查询用户数量
+        count = User.objects.filter(username=username).count()
+        return Response({
+            'username': username,
+            'count': count,
+        })
+
+
+# 判断手机号是否存在
+class MobileCountView(APIView):
+    def get(self, request, mobile):
+        count = User.objects.filter(mobile=mobile).count()
+        return Response({
+            'mobile': mobile,
+            'count': count,
+        })
+
+
+# 注册保存数据
+class UserView(CreateAPIView):
+    serializer_class = UserSerializer
+    # 获取前段数据
+    # 校验数据
+    # 保存数据
+    # 返回结果
